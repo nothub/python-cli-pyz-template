5d0a5899245120199183d23a2fe8e37cb181922a
nothub <48992448+nothub@users.noreply.github.com>
2021-06-18 23:54:52 +0200

    demo code

--- Makefile
+++ Makefile
@@ -14,3 +14,8 @@ requirements: $(eval SHELL:=/bin/bash) virtualenv requirements.txt
 run: $(eval SHELL:=/bin/bash) requirements
 	source venv/bin/activate
 	python3 main.py
+
+package: $(eval SHELL:=/bin/bash) requirements
+	source venv/bin/activate
+	pip3 install zipapps
+	python3 -m zipapps -c -a main.py -m main -o app -r requirements.txt -p "/usr/bin/env python3"
--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 #!/usr/bin/env python3
-import this
+import random
+import time
+import uuid
+from random import randrange
 
 from rich import inspect
+from rich.console import Console
+from rich.live import Live
+from rich.progress import track
+from rich.tree import Tree
 
-inspect(this)
+console = Console()
+
+emojis = [
+    ':baby_chick:',
+    ':cow:',
+    ':dog:',
+    ':elephant:',
+    ':gorilla:',
+    ':monkey:',
+    ':see_no_evil:',
+    ':hear_no_evil:',
+    ':speak_no_evil:',
+    ':ox:',
+    ':pig:',
+    ':ram:',
+    ':rat:',
+    ':tiger:',
+]
+
+
+def hack(cia: Tree) -> Tree:
+    node = cia.add(uuid.uuid4().hex)
+    for i in range(randrange(3)):
+        sub_node = node.add(cia.add(uuid.uuid4().hex))
+        if not random.getrandbits(1):
+            for j in range(randrange(5)):
+                sub_node.add(cia.add(uuid.uuid4().hex))
+    return cia
+
+
+tree = Tree("heck CIA")
+with Live(hack(tree), refresh_per_second=4) as live:
+    for _ in range(4):
+        time.sleep(0.5)
+        live.update(hack(tree))
+
+console.print(''.join([random.choice(emojis) for emoji in emojis]))
+
+for n in track(range(42), description="dumping..."):
+    time.sleep(0.1)
+
+import this
+
+inspect(this, title='Zen', docs=True)
