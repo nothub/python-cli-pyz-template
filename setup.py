e51381e960b63e13270d4f33887df1869001473f
nothub <48992448+nothub@users.noreply.github.com>
2021-06-20 23:52:45 +0200

    infos

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='python-cli-template',
-    description='template for python cli apps',
-    version='0.0.0',
+    name='python-cli-pyz-template',
+    description='template for python cli apps packagable as pyz archives',
+    version='0.0',
     author='nothub',
     license='MIT',
     packages=[''],
